c545f81ad1e93380512110c028140d043ea56e09
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2023-10-23 12:29:02 +0300

    add layouts start states in GroupForm.java

--- src/Forms/GroupForm.java
+++ src/Forms/GroupForm.java
@@ -1,21 +1,30 @@
 package Forms;
 
-import customComponents.*;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class GroupForm extends JFrame {
-
+    private JPanel mainPanel = new JPanel();
+    private JPanel innerLeftLayoutPanel = new JPanel();
+    private JPanel innerUpLayoutPanel = new JPanel();
+    private JPanel contentLayoutPanel = new JPanel();
     public GroupForm(){
-
+        setLayouts();
+        setStartFormState();
     }
 
-    private void setLayout(){
+    private void setLayouts(){
+        mainPanel.setLayout(new BorderLayout());
+        innerLeftLayoutPanel.setLayout(new BoxLayout(innerLeftLayoutPanel, BoxLayout.Y_AXIS));
+        innerUpLayoutPanel.setLayout(new BoxLayout(innerUpLayoutPanel, BoxLayout.X_AXIS));
+        contentLayoutPanel.setLayout(new BoxLayout(contentLayoutPanel, BoxLayout.Y_AXIS));
 
+        mainPanel.add(innerUpLayoutPanel, BorderLayout.NORTH);
+        mainPanel.add(innerLeftLayoutPanel, BorderLayout.EAST);
+        mainPanel.add(contentLayoutPanel, BorderLayout.CENTER);
     }
 
-    private void setInLayouts(){
+    private void setStartFormState(){
 
     }
 
