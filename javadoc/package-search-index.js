46ede3770c43b4e7787fe4448435b9bd696f8314
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-03-05 23:11:47 +0300

    documentation and libs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"CustomComponents"},{"l":"CustomComponents.CustomTableActionCells"},{"l":"Database.DAOS"},{"l":"Database.Managers"},{"l":"Frames"},{"l":"Frames.models"},{"l":"StudentGroupAppStarter"},{"l":"TableExport"}];updateSearchResults();
